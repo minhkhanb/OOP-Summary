922b01250f4fef873352687551e7c5f05354e1c9
Marc <minhkhanb@gmail.com>
2021-07-25 11:55:17 +0700

    opengl

---
+++ Opengl/CMakeFiles/CoHangXom3D.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CoHangXom3D.dir/CoHangXom3D.cpp.obj"
+  "CMakeFiles/CoHangXom3D.dir/CoHangXom3D.cpp.obj.d"
+  "CoHangXom3D.exe"
+  "CoHangXom3D.exe.manifest"
+  "CoHangXom3D.pdb"
+  "libCoHangXom3D.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CoHangXom3D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Opengl/CMakeFiles/CoHangXom3D.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CoHangXom3D.
+# This may be replaced when dependencies are built.
