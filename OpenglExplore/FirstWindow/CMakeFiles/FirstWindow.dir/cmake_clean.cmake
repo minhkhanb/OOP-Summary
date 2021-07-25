22a0a769e23ae6090718a26f9078c933fcd96877
Marc <minhkhanb@gmail.com>
2021-07-25 15:37:49 +0700

    opengl

---
+++ OpenglExplore/FirstWindow/CMakeFiles/FirstWindow.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FirstWindow.dir/FirstWindow.cpp.obj"
+  "CMakeFiles/FirstWindow.dir/FirstWindow.cpp.obj.d"
+  "FirstWindow.exe"
+  "FirstWindow.exe.manifest"
+  "FirstWindow.pdb"
+  "libFirstWindow.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FirstWindow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
